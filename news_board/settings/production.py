24b417d911c08da3047c9403c1b3ad9f79c04ea2
Elena <lenasharabura@gmail.com>
2021-10-29 18:02:19 +0300

    add settings for Heroku

--- news_board/settings/production.py
+++ news_board/settings/production.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'news_board.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': django.db.backends.postgresql_psycopg2,
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
